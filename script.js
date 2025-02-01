be7016c24d3ebe79f53a6729960a402e895bb4f5
luter <newgglutt@gmail.com>
2025-02-01 14:28:29 +0200

    dev

---
+++ script.js
@@ -0,0 +1,11 @@
+{/* <script>
+    document.querySelectorAll('.select-spawn').forEach(button  {
+        button.addEventListener('click', () => {
+            alert('Ви натиснули кнопку!');
+            button.classList.add('clicked'); // Додаємо клас для анімації
+            setTimeout(() => {
+                button.classList.remove('clicked'); // Видаляємо клас через 300 мс
+            }, 300);
+        });
+    });
+</script> */}
